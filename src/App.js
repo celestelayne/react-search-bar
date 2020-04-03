2efab4c34d2e22961d25e580a677757fc1edd09b
Celeste Layne <celeste.layne@generalassemb.ly>
2020-04-02 20:53:16 -0400

    update readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let artists = [
+  {
+    name: 'Pablo Picasso'
+  },
+  {
+    name: 'Vincent Van Gogh'
+  },
+  {
+    name: 'Claude Monet'
+  },
+  {
+    name: 'Salvador Dali'
+  },
+]
+
+class App extends Component {
+  state = {
+    searchQuery: '',
+    initialState: artists,
+    searchedData: []
+  }
+
+  handleSearch = (e) => {
+    const searchQuery = e.target.value && e.target.value.toLowerCase()
+    console.log(searchQuery)
+
+    this.setState(prevState => {
+      const searchedData = prevState.initialState.filter(item => {
+        return item.name.toLowerCase().includes(searchQuery)
+      })
+      return {
+        searchQuery,
+        searchedData
+      }
+    })
+
+  }
+
+  render(){
+
+    let searched = this.state.searchQuery === "" ?
+       this.state.initialState && this.state.initialState.map((artist, index) => {
+        return (
+          <p key={index}>{artist.name}</p>
+        )
+      })
+       :
+      this.state.searchedData && this.state.searchedData.map((artist, index) => {
+        return (
+          <p key={index}>{artist.name}</p>
+        )
+      })
+
+    return (
+      <div className="App">
+        <header className="">
+          <input
+            type="text"
+            placeholder="Search for..."
+            onChange={this.handleSearch}
+            id="search"
+            name="search" />
+        </header>
+        <main>
+          {searched}
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default App
